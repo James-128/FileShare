bf413dc3450869dcfdb021c1491ae164532f6453
jmorey <jmorey551@gmail.com>
2023-05-19 21:20:32 -0500

    Organized folder structure

---
+++ src/GUI/GUI.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javax.swing.*;
+public class GUI {
+    public GUI () {
+        // create a frame
+        JFrame f = new JFrame("Menu demo");
+
+        GUIMenu t = new GUIMenu();
+
+        // add menubar to frame
+        f.setJMenuBar(t.menubar);
+
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+    }
+}
